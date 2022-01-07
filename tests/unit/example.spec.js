994aef83d688dd09d509c1a4f040ffa24a3210f2
Jakub Zomerfeld <dev.zomerfeld@gmail.com>
2022-01-07 11:26:45 +0100

    fix: issue with not triggered click.event

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
 describe('HelloWorld.vue', () => {
-  it('increments the counter when the button is clicked', () => {
-    const wrapper = shallowMount(HelloWorld)
+  it('increments the counter when the button is clicked', async () => {
+    const wrapper = mount(HelloWorld)
 
     for (let i = 0; i < 10; i++) {
-      wrapper.find('button').trigger('click');
+      await wrapper.find('button').trigger('click');
     }
 
     expect(wrapper.find('h1').text()).toBe("10");
